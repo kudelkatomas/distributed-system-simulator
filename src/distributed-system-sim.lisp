46884651d0fc1e04eff0423e0d3bb44e27a1e39c
Tomas Kudelka <tom.kudelka3@gmail.com>
2026-09-11 11:45:23 +0200

    Keep only the basics of the simulator and translate comments to english

--- src/distributed-system-sim.lisp
+++ src/distributed-system-sim.lisp
@@ -1,61 +1,21 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;
-;;; Simulátor distribuovaného systému
-;;;
-;;; Tomáš Kudělka
-;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;
+;;;; File: distributed-system-sim.lisp
+;;;; Author: Tomáš Kudělka
+;;;;
+;;;; Description:
+;;;;   Definitions of base classes: Message, Node, Network
+;;;;
 
 (require "asdf")
 (asdf:load-system :bordeaux-threads)
 
 (defparameter *broadcast* 0)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Pomocné funkce a makra
-;;
-
-; (n n+1 ... m)
-(defun range (n m)
-  (if (> n m)
-      nil
-    (cons n (range (1+ n) m))))
-
-(defun first-half (n)
-  (range 1 (floor (/ n 2))))
-
-(defun second-half (n)
-  (range (1+ (floor (/ n 2))) n))
-
-(defun replace-element (list el new-el &key (key (lambda (x) x)))
-  (cond ((null list) '())
-        ((eql el (funcall key (car list)))
-         (cons new-el (replace-element (cdr list) el new-el :key key)))
-        (t (cons (car list)
-                 (replace-element (cdr list) el new-el :key key)))))
-
-; Převzato z: https://lispcookbook.github.io/cl-cookbook/process.html
-(defmacro until (condition &body body)
-  "Loops around until the condition becomes true."
-  (let ((block-name (gensym)))
-    `(block ,block-name
-       (loop
-          (if ,condition
-              (return-from ,block-name nil)
-            (progn ,@body))))))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Message
-;; (sender-id receiver-id timestamp content)
-;;
-;; Pokud (= receiver-id *broadcast*) pak jde o broadcast
-;;
-;; Protokol
-;; :connection-request | :introduction | :resource-request | :resource-request-ack |
-;; | :resource-release
-;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Message
+;;;
 
 (defclass message ()
   ((sender-id :initarg :sender-id
@@ -71,24 +31,13 @@
             :initform (error "Message must have content.")
             :reader content)))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Node
-;; (id running-p message-queue program lambda-thread network node-ids clock
-;;  resource-state request-timestamp request-queue)
-;;
-;; V "program" je "this-node" navázano na dany uzel.
-;;
-;; node-ids = ((node-id . latest-time) ...)
-;;
-;; resource-state = :requested | :granted | :released
-;; "released" je výchozí hodnota
-;;
-;; request-queue = ((requester-id . timestamp) ...)
-;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Node
+;;;
 
 (defmacro node-program (&body body)
+  "THIS-NODE is bound to the node in BODY."
   `(lambda (this-node)
      ,@body))
 
@@ -103,43 +52,35 @@
    (message-queue-lock :initform (bt:make-lock))
    (program :initarg :program
             :initform (error "Node must have a program."))
-   (lambda-thread :initform nil)
+   (thread :initform nil)
    (network :initarg :network
             :initform (error "Node must be connected to some network.")
             :reader network)
-   (node-ids :initform nil)
+   (known-node-ids :initform nil)
    (clock :initform 0
-          :reader clock)
-   (resource-state :initform :released
-                   :accessor resource-state)
-   (request-timestamp :initform 0
-                      :accessor request-timestamp)
-   (request-queue :initform nil
-                  :accessor request-queue)))
+          :reader clock)))
 
-;; Before concurrency
-(defmethod initialize-instance ((nd node) &key)
-  (call-next-method)
-  (initialize-lambda-thread nd)
-  (join (slot-value nd 'network) nd))
-
-;; Before concurrency
-(defmethod initialize-lambda-thread ((nd node))
-  (setf (slot-value nd 'lambda-thread)
-        (lambda ()
-          (bt:make-thread
-           (lambda ()
-             (progn
-               (funcall (slot-value nd 'program) nd)
-               (bt:with-lock-held ((slot-value nd 'running-p-lock))
-                 (setf (slot-value nd 'running-p) nil))))))))
-
-;; Before concurrency
+; Not local
 (defmethod start ((nd node))
   (bt:with-lock-held ((slot-value nd 'running-p-lock))
     (unless (running-p nd)
       (setf (slot-value nd 'running-p) t)
-      (funcall (slot-value nd 'lambda-thread)))))
+      (setf (slot-value nd 'thread)
+            (bt:make-thread
+             (lambda ()
+               (unwind-protect
+                   (progn
+                     (join (network nd) nd)
+                     (funcall (slot-value nd 'program) nd))
+                 (unwind-protect
+                     (leave (network nd) nd)
+                   (bt:with-lock-held ((slot-value nd 'running-p-lock))
+                     (setf (slot-value nd 'running-p) nil))))
+               :name (format nil "node-~a" (id nd))))))))
+
+;;;
+;;; Message queue
+;;;
 
 ; Not local
 (defmethod enqueue-message ((nd node) (msg message))
@@ -151,53 +92,55 @@
                       (list msg)))))))
 
 ; Local
-(defmethod increment-clock ((nd node))
-  (incf (slot-value nd 'clock)))
-
-; Local
 (defmethod dequeue-message ((nd node))
   (bt:with-lock-held ((slot-value nd 'message-queue-lock))
     (when (slot-value nd 'message-queue)
       (pop (slot-value nd 'message-queue)))))
 
 ; Local
-(defmethod handle-message ((nd node))
+(defmethod process-next-message ((nd node))
   (let ((msg (dequeue-message nd)))
     (when msg
-      (let ((sender-id (sender-id msg))
-            (timestamp (timestamp msg))
-            (content   (content msg)))
-        (lamport-update-clock nd timestamp)
-        (case (read-from-string content)
-          (:connection-request (handle-connection-request nd sender-id timestamp))
-          (:introduction (connect nd sender-id timestamp))
-          (:resource-request (handle-resource-request nd sender-id timestamp))
-          (:resource-release (request-queue-remove nd sender-id)))
-        (update-node-ids-clocks nd sender-id timestamp)))))
+      (let ((msg-type (read-from-string (content msg))))
+        (handle-message nd msg-type msg)))))
 
-; Local - Inkrementace hodin po
-(defmethod lamport-update-clock ((nd node) timestamp)
-  (setf (slot-value nd 'clock)
-        (1+ (max (clock nd) timestamp))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Message handling
+;;;
+;;; Message types:
+;;;   :connection-request |
+;;;   :introduction |
+;;;   :resource-request |
+;;;   :resource-request-ack |
+;;;   :resource-release
+;;;
+;;; Broadcast: (= receiver-id *broadcast*)
+;;;
 
-; Local
-(defmethod handle-connection-request ((nd node) requester-id timestamp)
-  (connect nd requester-id timestamp)
-  (send-message-as nd requester-id ":introduction"))
-
-; Local
-(defmethod handle-resource-request ((nd node) requester-id timestamp)
-  (enqueue-request nd requester-id timestamp)
-  (send-message-as nd requester-id ":resource-request-ack"))  
+(defgeneric handle-message (node message-type msg)
+  (:documentation "Handle MSG directed to NODE based on its MESSAGE-TYPE."))
 
 ; Local
-(defmethod connect ((nd node) sender-id timestamp)
-  (unless (member sender-id (slot-value nd 'node-ids) :key #'car)
-    (push (cons sender-id timestamp) (slot-value nd 'node-ids))))
+(defmethod handle-message ((nd node) (type (eql :connection-request)) msg)
+  (let ((requester-id (sender-id msg)))
+    (connect nd requester-id)
+    (send-message-as nd requester-id ":introduction")))
 
-; Local - Inkrementace hodin před
+; Local
+(defmethod handle-message ((nd node) (type (eql :introduction)) msg)
+  (connect nd (sender-id msg)))
+
+;;;
+;;; Helper methods
+;;;
+
+; Local
+(defmethod connect ((nd node) sender-id)
+  (pushnew sender-id (slot-value nd 'known-node-ids)))
+
+; Local
 (defmethod send-message-as ((nd node) receiver-id content)
-  (increment-clock nd)
   (send-message (network nd)
                 (make-instance 'message
                                :sender-id (id nd)
@@ -205,77 +148,18 @@
                                :content content
                                :timestamp (clock nd))))
 
-; Local
-(defmethod update-node-ids-clocks ((nd node) node-id timestamp)
-  (let ((pair (find node-id (slot-value nd 'node-ids) :key #'car)))
-    (when (and pair
-               (> timestamp (cdr pair)))
-      (setf (slot-value nd 'node-ids)
-            (replace-element (slot-value nd 'node-ids)
-                             node-id
-                             (cons node-id timestamp)
-                             :key #'car)))))
-
-; Local
-(defmethod request-resource ((nd node))
-  (setf (resource-state nd) :requested)
-  (send-message-as nd *broadcast* ":resource-request")
-  (setf (request-timestamp nd) (clock nd))
-  (enqueue-request nd (id nd) (clock nd)))
-
-; Local
-(defmethod release-resource ((nd node))
-  (setf (resource-state nd) :released)
-  (request-queue-remove nd (id nd))
-  (send-message-as nd *broadcast* ":resource-release"))
-
-; Local
-(defmethod enqueue-request ((nd node) requester-id timestamp)
-  (push (cons requester-id timestamp) (request-queue nd)))
-
-; Local
-(defmethod request-queue-head ((nd node))
-  (reduce (lambda (min-pair pair)
-            (cond ((< (cdr pair) (cdr min-pair)) pair)
-                  ((and (= (cdr pair) (cdr min-pair))
-                        (< (car pair) (car min-pair))) pair)
-                  (t min-pair)))
-          (request-queue nd)))
-
-; Local
-(defmethod request-queue-remove ((nd node) releaser-id)
-  (setf (request-queue nd)
-        (remove-if (lambda (node-id) (eql node-id releaser-id))
-                   (request-queue nd)
-                   :key #'car)))
-
-; Local
-(defmethod holds-resource-p ((nd node))
-  (or (eql (resource-state nd) :granted)
-      (and (= (id nd)                    ; 5.(i) z Lamportova textu
-              (car (request-queue-head nd)))
-           (< (request-timestamp nd)     ; 5.(ii) z Lamportova textu
-              (oldest-latest-received-message-timestamp nd))
-           (setf (resource-state nd) :granted))))
-
-; Local
-(defmethod oldest-latest-received-message-timestamp ((nd node))
-  (reduce #'min (slot-value nd 'node-ids) :key #'cdr))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Network
-;; (nodes)
-;;
-;; Uchovává si seznam uzlů, které se připojily, a rozesílá zprávy.
-;; Pokud (= receiver-id *broadcast*) pak jde o broadcast
-;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Network
+;;;
+;;; Stores a list of connected nodes and distributes messages.
+;;;
+;;; Broadcast: (= receiver-id *broadcast*)
+;;;
 
 (defclass network ()
   ((nodes :initform nil)
-   (nodes-lock :initform (bt:make-lock))
-   (sending-lock :initform (bt:make-lock))))
+   (nodes-lock :initform (bt:make-lock))))
 
 (defmethod nodes ((nw network))
   (bt:with-lock-held ((slot-value nw 'nodes-lock))
@@ -284,6 +168,11 @@
 (defmethod join ((nw network) (nd node))
   (bt:with-lock-held ((slot-value nw 'nodes-lock))
     (push nd (slot-value nw 'nodes))))
+
+(defmethod leave ((nw network) (nd node))
+  (bt:with-lock-held ((slot-value nw 'nodes-lock))
+    (setf (slot-value nw 'nodes)
+          (remove nd (slot-value nw 'nodes)))))
 
 (defmethod send-message ((nw network) (msg message))
   (labels ((broadcast-message ()
@@ -302,145 +191,3 @@
     (if (= (receiver-id msg) *broadcast*)
         (broadcast-message)
       (direct-message))))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Ukázka
-;;
-;; Funkce "run-program" a "run-2-programs" prepisují "network", "nodes" a "threads".
-;; Funkce "run-2-programs" přepisuje také "nodes-2".
-;;
-
-(defparameter *node-count* 20)
-
-(defparameter *program* nil)
-(defparameter *program-not-synced* nil)
-(defparameter *program-synced* nil)
-
-(defparameter *program-1* nil)
-(defparameter *program-2* nil)
-
-(defparameter *network* nil)
-
-(defparameter *nodes* nil)
-(defparameter *nodes-2* nil)
-
-(defparameter *threads* nil)
-
-;;
-;; Programy
-;;
-
-#|
-;; Jedna skupina
-(setf *program*
-      (node-program
-        (send-message-as this-node *broadcast* ":connection-request")
-        (loop repeat 100 do
-                (sleep 0.01)
-                (handle-message this-node))))
-
-
-;; Dve skupiny
-(setf *program-1*
-      (node-program
-        (send-message-as this-node *broadcast* ":connection-request")
-        (loop repeat 100 do
-                (sleep 0.01)
-                (handle-message this-node)))
-      
-      *program-2*
-      (node-program
-        (send-message-as this-node *broadcast* ":connection-request")
-        (loop repeat 100 do
-                (sleep 0.01)
-                (handle-message this-node))))
-|#
-
-;; Vzajemne vylouceni
-
-; Bez synchronizace
-(setf *program-not-synced*
-      (node-program
-        (send-message-as this-node *broadcast* ":connection-request")
-        (loop repeat 100 do
-              (sleep 0.01)
-              (handle-message this-node))
-
-        (format t "[id: ~a; time:~a]~%" (id this-node) (clock this-node))))
-
-; Se synchronizací
-(setf *program-synced*
-      (node-program
-        (send-message-as this-node *broadcast* ":connection-request")
-        (loop repeat 100 do
-                (sleep 0.01)
-                (handle-message this-node))
-
-        (loop repeat 5 do
-                (request-resource this-node)
-                (until (holds-resource-p this-node)
-                       (handle-message this-node))
-                (format t "[id: ~a; request-time:~a]~%"
-                        (id this-node) (request-timestamp this-node))
-                (release-resource this-node))))
-     
-
-;;
-;; Spuštění naráz
-;;
-
-(defun run-program (program)
-  (progn
-    (setf *network* (make-instance 'network)
-          *nodes*   (mapcar (lambda (i)
-                              (make-instance 'node
-                                             :id i
-                                             :program program
-                                             :network *network*))
-                            (range 1 *node-count*))
-          *threads* (mapcar #'start *nodes*))
-    (pprint *threads*)))
-
-;;
-;; Spuštění po dvou skupinach
-;;
-
-(defun run-2-programs (program-1 program-2)
-  (progn
-    (setf *network*                (make-instance 'network)
-          *nodes*                  (mapcar (lambda (i)
-                                             (make-instance 'node
-                                                            :id i
-                                                            :program program-1
-                                                            :network *network*))
-                                           (first-half *node-count*))
-          *threads*                (mapcar #'start *nodes*))
-    (format t "First group started...~%")
-    (sleep 0.5)
-    (setf *nodes-2*                (mapcar (lambda (i)
-                                             (make-instance 'node
-                                                            :id i
-                                                            :program program-2
-                                                            :network *network*))
-                                           (second-half *node-count*))
-          (cdr (last *threads*))   (mapcar #'start *nodes-2*)
-          (cdr (last *nodes*))     *nodes-2*)
-    (format t "Both groups started.~%")))
-
-;;
-;; Pomocná funkce pro počkání než všechny uzly dopočítají
-;;
-
-(defun wait-for-all ()
-  (format t "Waiting for threads to finish computing...~%")
-  (mapcar #'bt:join-thread *threads*)
-  (format t "All threads finished computing.~%"))
-
-;;
-;; Pomocná funkce pro získání hodnot hodin uzlů (nepouziva lock)
-;;
-
-(defun clocks ()
-  (mapcar #'clock *nodes*))
---
+++ src/helpers.lisp
@@ -0,0 +1,37 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;
+;;;; File: helpers.lisp
+;;;; Author: Tomáš Kudělka
+;;;;
+;;;; Description:
+;;;;   Helper functions and macros
+;;;;
+
+(defun range (from to)
+  "Returns a list of numbers (from from+1 ... to)."
+  (if (> from to)
+      nil
+    (cons from (range (1+ from) to))))
+
+(defun first-half (n)
+  (range 1 (floor (/ n 2))))
+
+(defun second-half (n)
+  (range (1+ (floor (/ n 2))) n))
+
+(defun replace-element (list el new-el &key (key (lambda (x) x)))
+  (cond ((null list) '())
+        ((eql el (funcall key (car list)))
+         (cons new-el (replace-element (cdr list) el new-el :key key)))
+        (t (cons (car list)
+                 (replace-element (cdr list) el new-el :key key)))))
+
+; Source: https://lispcookbook.github.io/cl-cookbook/process.html
+(defmacro until (condition &body body)
+  "Loops around until the condition becomes true."
+  (let ((block-name (gensym)))
+    `(block ,block-name
+       (loop
+          (if ,condition
+              (return-from ,block-name nil)
+            (progn ,@body))))))
