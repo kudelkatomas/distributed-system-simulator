ab1b0dece27a6d02c7bbe40e9c5595fedd361fbe
Tomas Kudelka <tom.kudelka3@gmail.com>
2026-09-18 22:40:47 +0200

    Update mutex-lamport.lisp

--- src/mutex-lamport.lisp
+++ src/mutex-lamport.lisp
@@ -4,24 +4,33 @@
 ;;;; Author: Tomáš Kudělka
 ;;;;
 ;;;; Description:
-;;;;   Shared resource access mutual exlusion using Lamport's system of logical clocks
-;;;;   Lamport's paper: https://doi.org/10.1145/359545.359563
+;;;;   Mutual exclusion for shared resource access using
+;;;;   Lamport's system of logical clocks: https://doi.org/10.1145/359545.359563
+;;;;
+;;;; Assumptions:
+;;;;   For mutual exclusion to work, each node must be connected to every other node.
+;;;;   Precisely, in HOLDS-RESOURCE-P, it is assumed that the given node has received
+;;;;   at least one message from every other node.
 ;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Lamport node
 ;;;
-;;; The node class extension enabling mutual exclusion
+;;; Subclass of NODE implementing mutual exclusion.
+;;;
+;;; Possible resource states are :requested, :granted, :released.
+;;; Default state is :released.
 ;;;
 
 (defclass lamport-node (node)
   ((resource-state :initform :released
-                   :accessor resource-state)
+                   :reader resource-state
+                   :documentation "Possible states are :requested, :granted, :released. Default state is :released.")
    (request-timestamp :initform 0
-                      :accessor request-timestamp)
+                      :reader request-timestamp)
    (request-queue :initform nil
-                  :accessor request-queue)
+                  :reader request-queue)
    (known-node-clocks :initform nil
                       :reader known-node-clocks
                       :documentation "List of pairs (id . id's clock value)")))
@@ -31,54 +40,72 @@
 ;;;
 
 ;; Local
+;; Notice that (clock nd) after sending the request message
+;; is equal to the timestamp sent by send-message-as,
+;; since clock value can only be changed locally,
+;; and no local operation can interfere with request-resource.
 (defmethod request-resource ((nd lamport-node))
-  (setf (resource-state nd) :requested)
+  (setf (slot-value nd 'resource-state) :requested)
   (send-message-as nd *broadcast* ":resource-request")
-  (setf (request-timestamp nd) (clock nd))
-  (enqueue-request nd (id nd) (clock nd)))
+  (let ((timestamp (clock nd)))
+    (setf (slot-value nd 'request-timestamp) timestamp)
+    (enqueue-request nd (id nd) timestamp))
+  nd)
 
 ;; Local
 (defmethod release-resource ((nd lamport-node))
-  (setf (resource-state nd) :released)
+  (setf (slot-value nd 'resource-state) :released)
   (request-queue-remove nd (id nd))
-  (send-message-as nd *broadcast* ":resource-release"))
+  (send-message-as nd *broadcast* ":resource-release")
+  nd)
 
 ;; Local
 (defmethod holds-resource-p ((nd lamport-node))
+  "Check if ND holds the resource according to Lamport's conditions 5.(i) and 5.(ii). Assumes ND has received at least one message from every other node in the network."
   (or (eql (resource-state nd) :granted)
-      (and (= (id nd)                    ; 5.(i) from Lamport's paper
-              (car (request-queue-head nd)))
-           (< (request-timestamp nd)     ; 5.(ii) from Lamport's paper
-              (oldest-latest-received-message-timestamp nd))
-           (setf (resource-state nd) :granted))))
+      (let ((request-queue-head (request-queue-head nd)))
+        (when (and request-queue-head
+                   ; 5.(i) from Lamport's paper
+                   (= (id nd) (car request-queue-head))
+                   ; 5.(ii) from Lamport's paper
+                   (all-other-clocks-greater-than-request-timestamp-p nd))
+          (setf (slot-value nd 'resource-state) :granted)
+          t))))
 
 ;; Local
-(defmethod oldest-latest-received-message-timestamp ((nd lamport-node))
-  (reduce #'min (slot-value nd 'node-ids) :key #'cdr))
+(defmethod all-other-clocks-greater-than-request-timestamp-p ((nd lamport-node))
+  "Check Lamport's condition 5.(ii). Assumes ND has received at least one message from every other node in the network."
+  (let ((known-node-clocks (known-node-clocks nd)))
+    (or (null known-node-clocks) ; ND is alone in the network.
+        (< (request-timestamp nd)
+           (reduce #'min known-node-clocks :key #'cdr)))))
 
 ;;;
-;;; Request queue
+;;; Request queue operations
 ;;;
 
 ;; Local
 (defmethod enqueue-request ((nd lamport-node) requester-id timestamp)
-  (push (cons requester-id timestamp) (request-queue nd)))
+  (push (cons requester-id timestamp) (slot-value nd 'request-queue))
+  nd)
 
 ;; Local
 (defmethod request-queue-head ((nd lamport-node))
-  (reduce (lambda (min-pair pair)
-            (cond ((< (cdr pair) (cdr min-pair)) pair)
-                  ((and (= (cdr pair) (cdr min-pair))
-                        (< (car pair) (car min-pair))) pair)
-                  (t min-pair)))
-          (request-queue nd)))
+  (when (request-queue nd)
+    (reduce (lambda (min-pair pair)
+              (cond ((< (cdr pair) (cdr min-pair)) pair)
+                    ((and (= (cdr pair) (cdr min-pair))
+                          (< (car pair) (car min-pair))) pair)
+                    (t min-pair)))
+            (request-queue nd))))
 
 ;; Local
 (defmethod request-queue-remove ((nd lamport-node) releaser-id)
-  (setf (request-queue nd)
+  (setf (slot-value nd 'request-queue)
         (remove-if (lambda (node-id) (eql node-id releaser-id))
                    (request-queue nd)
-                   :key #'car)))
+                   :key #'car))
+  nd)
 
 ;;;
 ;;; Known nodes clocks
@@ -86,23 +113,12 @@
 
 ;; Local
 (defmethod update-known-node-clocks ((nd lamport-node) sender-id timestamp)
-  (let ((pair (find sender-id (known-node-clocks nd) :key #'car)))
-    (cond ((and pair (> timestamp (cdr pair)))
-           (setf (slot-value nd 'known-node-clocks)
-                 (replace-element (slot-value nd 'known-node-clocks)
-                                  sender-id
-                                  (cons sender-id timestamp)
-                                  :key #'car)))
-          ((not pair)
-           (push (cons sender-id timestamp) (slot-value nd 'known-node-clocks))))))
-
-;; Helper function
-(defun replace-element (list el new-el &key (key (lambda (x) x)))
-  (cond ((null list) '())
-        ((eql el (funcall key (car list)))
-         (cons new-el (replace-element (cdr list) el new-el :key key)))
-        (t (cons (car list)
-                 (replace-element (cdr list) el new-el :key key)))))
+  (unless (= sender-id (id nd))
+    (let ((pair (assoc sender-id (known-node-clocks nd))))
+      (if pair
+          (setf (cdr pair) (max (cdr pair) timestamp))
+        (push (cons sender-id timestamp) (slot-value nd 'known-node-clocks)))))
+  nd)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -113,14 +129,17 @@
 
 ;; Local
 (defmethod handle-message :after ((nd lamport-node) msg-type (msg message))
-  (update-known-node-clocks nd (sender-id msg) (timestamp msg)))
+  (update-known-node-clocks nd (sender-id msg) (timestamp msg))
+  nd)
 
 ;; Local
 (defmethod handle-message ((nd lamport-node) (msg-type (eql :resource-request)) (msg message))
   (let ((requester-id (sender-id msg)))
     (enqueue-request nd requester-id (timestamp msg))
-    (send-message-as nd requester-id ":resource-request-ack")))
+    (send-message-as nd requester-id ":resource-request-ack"))
+  nd)
 
 ;; Local
 (defmethod handle-message ((nd lamport-node) (msg-type (eql :resource-release)) (msg message))
-  (request-queue-remove nd (sender-id nd)))
+  (request-queue-remove nd (sender-id msg))
+  nd)
