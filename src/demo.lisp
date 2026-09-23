14e975783551a74e8b5d8b09890f1f24a5694a53
Tomas Kudelka <tom.kudelka3@gmail.com>
2026-09-23 20:05:03 +0200

    Add all examples to demo.lisp

--- src/demo.lisp
+++ src/demo.lisp
@@ -14,19 +14,18 @@
 ;;; Nodes will most likely know each other at the end.
 ;;;
 
-#|
-(let* ((node-count 3)
-       (program (node-program (this-node)
-                  (send-message-as this-node
-                                   *broadcast*
-                                   ":connection-request")
-                  (loop repeat node-count do
-                          (sleep 0.01)
-                          (process-next-message this-node))))
-       (nodes (start-n-nodes node-count 'node program)))
-  (wait-for-all nodes)
-  (mapcar #'known-node-ids nodes))
-|#
+(defun run-example-1 (&optional (node-count 3))
+  (let* ((program (node-program (this-node)
+                    (send-message-as this-node
+                                     *broadcast*
+                                     ":connection-request")
+                    (loop repeat node-count do
+                            (sleep 0.01)
+                            (process-next-message this-node))))
+         (nodes (start-n-nodes node-count 'node program)))
+    (wait-for-all nodes)
+    (format t "Will return a list of node-known-ids.~%")
+    (mapcar #'known-node-ids nodes)))
 
 ;;;
 ;;; Example 2
@@ -35,113 +34,79 @@
 ;;; Nodes will most likely NOT know each other at the end.
 ;;;
 
-#|
-(let* ((group-node-count 2)
-       (pause-between-starts-duration 0.5)
-       (program (node-program (this-node)
-                  (send-message-as this-node
-                                   *broadcast*
-                                   ":connection-request")
-                  (loop repeat (* 2 group-node-count) do
-                          (sleep 0.01)
-                          (process-next-message this-node))))
-       (groups (start-2n-nodes-in-two-groups group-node-count
-                                             'node
-                                             program
-                                             :sleep-duration pause-between-starts-duration))
-       (nodes (append (car groups) (cdr groups))))
-  (wait-for-all nodes)
-  (mapcar #'known-node-ids nodes))
-|#
+(defun run-example-2 (&optional (group-node-count 2) (pause-between-starts-duration 0.5))
+  (let* ((program (node-program (this-node)
+                    (send-message-as this-node
+                                     *broadcast*
+                                     ":connection-request")
+                    (loop repeat (* 2 group-node-count) do
+                            (sleep 0.01)
+                            (process-next-message this-node))))
+         (groups (start-2n-nodes-in-two-groups group-node-count
+                                               'node
+                                               program
+                                               :sleep-duration pause-between-starts-duration))
+         (nodes (append (car groups) (cdr groups))))
+    (wait-for-all nodes)
+    (format t "Will return a list of node-known-ids.~%")
+    (mapcar #'known-node-ids nodes)))
 
 ;;;
 ;;; Example 3
 ;;;
-;;; TO BE COMPLETED
-;;;
 ;;; Mutual exclusion.
 ;;; The resource is the ability to print to the output.
 ;;;
+;;; In this example we give the nodes access to node-count to ensure they know each other
+;;; before using the resource.
+;;; See Assumptions in mutex-lamport.lisp.
+;;;
 
-#| No synchronization |#
+;; No synchronization
+(defun run-example-3-no-sync (&optional (node-count 10) (prints-per-node 5))
+  (let* ((program (node-program (this-node)
+                    ; Introduction    
+                    (until (= (length (known-node-ids this-node))
+                              (1- node-count))
+                      (send-message-as this-node
+                                       *broadcast*
+                                       ":connection-request")
+                      (sleep 0.01)
+                      (process-next-message this-node))
+                    ; Using the resource
+                    (loop repeat prints-per-node do
+                            (format t "[id: ~a; time:~a]~%" (id this-node) (clock this-node)))))
+         (nodes (start-n-nodes node-count 'node program)))
+    (wait-for-all nodes)
+    (format t "Check the output.~%")))
 
-;; Vzajemne vylouceni
-
-; Bez synchronizace
-(setf *program-not-synced*
-      (node-program
-        (send-message-as this-node *broadcast* ":connection-request")
-        (loop repeat 100 do
-              (sleep 0.01)
-              (handle-message this-node))
-
-        (format t "[id: ~a; time:~a]~%" (id this-node) (clock this-node))))
-
-; Se synchronizací
-(setf *program-synced*
-      (node-program
-        (send-message-as this-node *broadcast* ":connection-request")
-        (loop repeat 100 do
-                (sleep 0.01)
-                (handle-message this-node))
-
-        (loop repeat 5 do
-                (request-resource this-node)
-                (until (holds-resource-p this-node)
-                       (handle-message this-node))
-                (format t "[id: ~a; request-time:~a]~%"
-                        (id this-node) (request-timestamp this-node))
-                (release-resource this-node))))
-     
-
-;;
-;; Spuštění naráz
-;;
-
-(defun run-program (program)
-  (progn
-    (setf *network* (make-instance 'network)
-          *nodes*   (mapcar (lambda (i)
-                              (make-instance 'node
-                                             :id i
-                                             :program program
-                                             :network *network*))
-                            (range 1 *node-count*))
-          *threads* (mapcar #'start *nodes*))
-    (pprint *threads*)))
-
-
-
-
-;;
-;; Spuštění po dvou skupinach
-;;
-
-(defun run-2-programs (program-1 program-2)
-  (progn
-    (setf *network*                (make-instance 'network)
-          *nodes*                  (mapcar (lambda (i)
-                                             (make-instance 'node
-                                                            :id i
-                                                            :program program-1
-                                                            :network *network*))
-                                           (first-half *node-count*))
-          *threads*                (mapcar #'start *nodes*))
-    (format t "First group started...~%")
-    (sleep 0.5)
-    (setf *nodes-2*                (mapcar (lambda (i)
-                                             (make-instance 'node
-                                                            :id i
-                                                            :program program-2
-                                                            :network *network*))
-                                           (second-half *node-count*))
-          (cdr (last *threads*))   (mapcar #'start *nodes-2*)
-          (cdr (last *nodes*))     *nodes-2*)
-    (format t "Both groups started.~%")))
-
-;;
-;; Pomocná funkce pro získání hodnot hodin uzlů (nepouziva lock)
-;;
-
-(defun clocks ()
-  (mapcar #'clock *nodes*))
+;; Synchronized (lamport-node)
+(defun run-example-3-sync (&optional (node-count 10) (prints-per-node 5))
+  (let* ((program (node-program (this-node)
+                    ; Introduction
+                    (until (= (length (known-node-ids this-node))
+                              (1- node-count))
+                      (send-message-as this-node
+                                       *broadcast*
+                                       ":connection-request")
+                      (sleep 0.01)
+                      (process-next-message this-node))
+                    ; Using the resource
+                    (loop repeat prints-per-node do
+                            (request-resource this-node)
+                            (until (holds-resource-p this-node)
+                              (process-next-message this-node))
+                            (format t "[id: ~a; time:~a]~%" (id this-node) (clock this-node))
+                            (release-resource this-node))
+                    ; Sending acknowledgements to others
+                    ;
+                    ; Deadlock occurs if any of the nodes shuts down too soon.
+                    ; Barrier should be used here to check that none of the nodes
+                    ; wants the resource anymore. Loop is used for simplicity,
+                    ; but does not prevent the deadlock in all cases.
+                    (loop repeat (* 2 node-count prints-per-node) do
+                            (sleep 0.01)
+                            (process-next-message this-node))))
+         (nodes (start-n-nodes node-count 'lamport-node program)))
+    (wait-for-all nodes)
+    (format t "Check the output.~%")))
